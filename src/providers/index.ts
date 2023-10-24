037897a9434e6538f1e29a12363e2871ecac849b
Mahesh Vagicherla <maheshmahi19@outlook.com>
2023-10-24 14:01:55 +0530

    update providers list

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -3,14 +3,16 @@ import AnyscaleConfig from "./anyscale";
 import AzureOpenAIConfig from "./azure-openai";
 import CohereConfig from "./cohere";
 import OpenAIConfig from "./openai";
+import ReplicateAIConfig from "./replicate";
 import { ProviderConfigs } from "./types";
 
 const Providers: { [key: string]: ProviderConfigs } = {
   openai: OpenAIConfig,
   cohere: CohereConfig,
   anthropic: AnthropicConfig,
-  'azure-openai': AzureOpenAIConfig,
-  anyscale: AnyscaleConfig
+  "azure-openai": AzureOpenAIConfig,
+  anyscale: AnyscaleConfig,
+  replicate: ReplicateAIConfig,
 };
 
 export default Providers;
